e7fd1fc04e096dc3018e15666aa6494a5d0fd867
Jeff <11721615+jeffellin@users.noreply.github.com>
2021-07-10 22:11:37 -0400

    update scripts

--- tools/scripts/getcicd.sh
+++ tools/scripts/getcicd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update these to match your environment
-export SERVICE_ACCOUNT_NAME=argo-sa
+export SERVICE_ACCOUNT_NAME=kpack-deploy-sa
 export NAMESPACE=default
-export NEW_CONTEXT=argo-sa
+export NEW_CONTEXT=kpack-deploy-sa
 
 ./getkube.sh $SERVICE_ACCOUNT_NAME $NAMESPACE $NEW_CONTEXT
